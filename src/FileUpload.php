faac8799ca0a3db8b2210c94407dbbb96e5291de
Václav Stodůlka <vsek@seznam.cz>
2015-12-03 11:11:47 +0100

    Přidán upload obrázku

---
+++ src/FileUpload.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Form;
+
+use \Nette\Utils\Html;
+
+/**
+ * Description of FileUpload
+ *
+ * @author Vsek
+ */
+class FileUpload extends \Nette\Forms\Controls\TextInput{
+    
+    private $imageOnly = true;
+    private $buttonText = 'Select file';
+    private $multiple = false;
+    
+    /**
+     * Nastavi text tlacitka
+     * @param string $buttonText
+     */
+    public function setButtonText($buttonText){
+        $this->buttonText = $buttonText;
+    }
+    
+    public function __construct($label = NULL, $multiple = false, $imageOnly = true) {
+        parent::__construct($label);
+        
+        $this->imageOnly = $imageOnly;
+        $this->multiple = $multiple;
+    }
+    
+    public function getControl() {
+        $input = parent::getControl();
+        $input->class = 'hidden';
+        
+        //$input = Html::el('input')->id($this->getHtmlId() . '_file');
+
+        $timestamp = time();
+        $input->timestamp($timestamp);
+        $input->token(md5('unique_salt' . $timestamp));
+        $input->upload($this->imageOnly ? '/js/uploadifive/uploadifive-image-only.php' : '/js/uploadifive/uploadifive.php');
+        $input->buttonText($this->buttonText);
+        $input->multi($this->multiple ? 'true' : 'false');
+        $input->class('uploadifive');
+
+        $control = Html::el()->add($input);
+        
+        //edit - zobrazim obrazek
+        if($this->getValue() != ''){
+            if(!$this->multiple){
+                if($this->imageOnly){
+                    $image = Html::el('img')->src($this->getParent()->getParent()->link('Image:preview', $this->getValue(), 100, 100));
+                    $control->add($image);
+                }else{
+                    $link = Html::el('a')->href('/images/upload/' . substr($this->getValue(), 0, 4) . '/' . $this->getValue())->setText($this->getValue());
+                    $control->add($link);
+                }
+            }else{
+                
+            }
+        }
+        
+        return $control;
+    }
+    
+    public function getValue() {
+        $value = parent::getValue();
+        if($this->multiple){
+            $images = array();
+            foreach(explode(',', $value) as $img){
+                if($img != ''){
+                    $images[] = $img;
+                }
+            }
+            return $images;
+        }else{
+            return $value;
+        }
+    }
+}
--- src/FormAdmin.php
+++ src/FormAdmin.php
@@ -20,7 +20,7 @@ class Form extends \Nette\Application\UI\Form{
     
     /**
      * 
-     * @return \App\Extensions\FormSpawEditor
+     * @return \App\Form\FormSpawEditor
      */
     public function addSpawEditor($name, $label = NULL, $cols = 40, $rows = 10, $type = 'all'){
         return $this[$name] = new \App\Form\FormSpawEditor($label, $cols, $rows, $type);
@@ -28,10 +28,22 @@ class Form extends \Nette\Application\UI\Form{
     
     /**
     * Adds control that allows the user to upload files.
-    * @return \App\Extensions\Date
+    * @return \App\Form\Date
     */
     public function addDate($name, $label = NULL, $dateFormatUI = 'd.m.yy', $dateFormat = 'j.n.Y')
     {
            return $this[$name] = new \App\Form\Date($label, $dateFormatUI, $dateFormat);
+    }
+    
+    /**
+    * Adds control that allows the user to upload files.
+    * @param  string  control name
+    * @param  string  label
+    * @param  bool  allows to upload multiple files
+    * @return Nette\Forms\Controls\UploadControl
+    */
+    public function addUpload($name, $label = NULL, $multiple = FALSE, $onlyImage = true)
+    {
+           return $this[$name] = new \App\Form\FileUpload($label, $multiple, $onlyImage);
     }
 }
